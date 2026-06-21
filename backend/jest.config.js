7921d7eaaf5c74ffae0f0b9e2a13e2f9301df102
xachilleas <xachilleas@gmail.com>
2026-06-21 13:26:16 +0300

    feat (backend): part of jest testing updates

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,11 +2,21 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     roots: ['<rootDir>/src'],
-    testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+    testMatch: ['**/__tests__/**/*.test.ts'],
     collectCoverageFrom: [
         'src/**/*.ts',
-        '!src/**/*.d.ts',
-        '!src/server.ts',
-        '!src/app.ts'
-    ]
+        '!src/**/*.test.ts',
+        '!src/**/__tests__/**',
+        '!src/index.ts',
+        '!src/swagger.ts',
+        '!src/test-*.ts'
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 50,
+            functions: 50,
+            lines: 50,
+            statements: 50
+        }
+    }
 };
